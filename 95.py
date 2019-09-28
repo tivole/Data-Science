cb23d426d43f515e04edd0d7eae3ab45ee4b383b
tivole <asgarovkamran@gmail.com>
2019-09-28 08:52:07 +0400

    Added 9.5 home work

---
+++ 95.py
@@ -0,0 +1,37 @@
+# (c) Tivole
+
+# Constanst
+NUMBER_ON_TABLE = 7 # Your ID number on group list
+N = 100 # How many number you want to take
+
+# Importing the libraries
+import pandas as pd
+import numpy as np
+
+# Importing dataset
+dataset = pd.read_csv('Random_Numbers.csv')
+Y = dataset.iloc[:N, NUMBER_ON_TABLE - 1].values
+Y.sort()
+
+# Distribution Function
+F = lambda x: x**2
+F_inv = lambda y: np.sqrt(y)
+
+# Calculation Xs
+X = np.array([F_inv(Y[i]) for i in range(len(Y))])
+
+# Kolmagorov's criterion
+max_val = max((1/N - F(X[0])), F(X[0]))
+for i in range(N):
+    possible_max_val = max(((i+1)/N - F(X[i])), (F(X[i]) - (i)/N))
+    max_val = max(max_val, possible_max_val)
+
+D_n = max_val
+a = np.sqrt(N) * D_n
+
+# Omega-square criterion
+b = 1/(12*N)
+
+for i in range(N):
+    b += (F(X[i]) - (2*(i+1) - 1)/(2*N))**2
+

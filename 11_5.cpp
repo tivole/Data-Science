600a4f043d45c2bd2ac59fdcfd3b3e07b86e3589
tivole <asgarovkamran@gmail.com>
2019-10-01 23:49:19 +0400

    Solved 11.5

---
+++ 11_5.cpp
@@ -0,0 +1,39 @@
+// (c) Tivole
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+#define N 100
+
+int F_emp(double *X, double x);
+
+int main() {
+
+    int i;
+    double X[N], R = 0;
+    FILE *f = fopen("input.txt", "r");
+
+    for (i = 0; i < N; i++) {
+        fscanf(f, "%lf", &X[i]);
+        X[i] = -log(1.0/X[i] - 1.0);
+    }
+
+    for (i = 0; i < N; i++) {
+        R += pow((1 - F_emp(X, X[i]) - F_emp(X, -X[i])), 2);
+    }
+
+    printf("R = %lf\n", R);
+
+    return 0;
+}
+
+int F_emp(double *X, double x) {
+    int value = 0;
+    for (int i = 0; i < N; i++)
+        if (X[i] <= x) value ++;
+    return (double) value / N;
+}

8556583215fbff15105f4aa5e45be00742891e44
tivole <asgarovkamran@gmail.com>
2020-02-15 00:56:23 +0400

    HomeworkL Add LECTURER Table from the Book.

---
+++ DataBase/LECTURER_Table.sql
@@ -0,0 +1,32 @@
+/*
+    SQL В ПРИМЕРАХ И ЗАДАЧАХ.
+    ISBN 985-475-004-3
+
+    Page 20. LECTURER Table.
+*/
+
+CREATE TABLE LECTURER (
+    LECTURER_ID INTEGER     NOT NULL Primary key,
+    SURNAME     VARCHAR(25) NOT NULL,
+    FIRSTNAME   VARCHAR(25), 
+    CITY        VARCHAR(25),
+    UNIV_ID     INTEGER
+);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID) 
+    VALUES (24, 'Колесников', 'Борис', 'Воронеж', 10);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID)
+    VALUES (46, 'Никонов', 'Иван', 'Воронеж', 10);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID)
+    VALUES (74, 'Лагутин', 'Павел', 'Москва', 2);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID)
+    VALUES (108, 'Струков', 'Николай', 'Москва', 22);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID)
+    VALUES (276, 'Николаев', 'Виктор', 'Воронеж', 10);
+
+INSERT INTO LECTURER (LECTURER_ID, SURNAME, FIRSTNAME, CITY, UNIV_ID)
+    VALUES (328, 'Сорокин', 'Андрей', 'Орел', 10);

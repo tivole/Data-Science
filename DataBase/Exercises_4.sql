38f3118c8c0e4924db4e4b3de0fd33f2d7534d8e
tivole <asgarovkamran@gmail.com>
2020-04-11 15:30:17 +0400

    Add solutions for DB

--- DataBase/Exercises_4.sql
+++ DataBase/Exercises_4.sql
@@ -4,3 +4,10 @@
     Exercises from page 45
 */
 
+-- 1
+SELECT COUNT(student_id) from EXAM_MARKS WHERE subj_id = 22;
+
+-- 2
+SELECT COUNT(DISTINCT subj_id) from EXAM_MARKS;
+
+-- 3
---
+++ DataBase/Exercises_5.sql
@@ -0,0 +1,14 @@
+/*
+    SQL В ПРИМЕРАХ И ЗАДАЧАХ.
+    ISBN 985-475-004-3
+    Exercises from page 50
+*/
+
+-- 1 (a)
+SELECT firstname, surname, stipend*1.2 from STUDENT ORDER BY stipend;
+
+-- 1 (b)
+SELECT firstname, surname, stipend*1.2 from STUDENT ORDER BY surname;
+
+
+-- 2 (a)

41b9c7f7b1460a8bc1fdf910a9cdec1a611d9a0b
Kamran <56604464+KmMik24@users.noreply.github.com>
2020-04-21 13:07:14 +0400

    Update Exercises_3.sql

--- DataBase/Exercises_3.sql
+++ DataBase/Exercises_3.sql
@@ -5,9 +5,25 @@
 */
 
 -- 1
-SELECT student_id||';'||UPPER(surname)||';'||UPPER(firstname)||';'||stipend||';'||UPPER(City)||';'||birthday||';'||univ_id from STUDENT;
+SELECT concat(student_id,';', UPPER(surname),';', UPPER(firstname),';', stipend,';', kurs,';', city,';', birthday,';', univ_id) FROM STUDENT;
 
 -- 2
-SELECT SUBSTR(firstname, 1, 1)||'.'||UPPER(surname)||';местожительства-'||UPPER(city)||';родился-'||birthday from STUDENT;
+SELECT CONCAT(SUBSTRING(firstname, 1, 1),'.', UPPER(surname),';', 'местожительства-', UPPER(city),';', 'родился-', CONVERT(Varchar(10), birthday, 105) ) FROM STUDENT;
 
 -- 3
+SELECT CONCAT(SUBSTRING(LOWER(firstname), 1, 1),'.', LOWER(surname),';', 'местожительства-', LOWER(city),';', 'родился:', CONVERT(Varchar(10), birthday, 106) ) FROM STUDENT;
+
+-- 4
+SELECT CONCAT(firstname,' ', surname, ' родился в ', year(birthday), ' году') FROM STUDENT;
+
+-- 5
+SELECT surname, firstname, stipend*100 FROM STUDENT;
+
+-- 6
+SELECT CONCAT(UPPER(firstname),' ', UPPER(surname), ' родился/ась в ', year(birthday), ' году') FROM STUDENT WHERE kurs IN (1,2,4);
+
+-- 7
+SELECT CONCAT('Код-', univ_id, ';', univ_name, '-г.   ', UPPER(city), ';', 'Рейтинг=', rating) FROM UNIVERSITY;
+
+-- 8
+SELECT CONCAT('Код-', univ_id, ';', univ_name, '-г.   ', UPPER(city), ';', 'Рейтинг=', ROUND(rating, -1)) FROM UNIVERSITY;
